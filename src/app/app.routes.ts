e7b288130308c9b23784880b3e4a7e9221ce6540
Mohamed Maiga <mohamed.maiga@ism.edu.sn>
2026-01-26 20:16:10 +0000

    Creation_integration et routage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { RvComponent } from './features/rv/rv.component';
+import { FormDemandeComponent } from './features/demande-rv/form-demande/form-demande.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+    }
+    ,{
+        path: 'dashboard',
+        component: DashboardComponent
+
+    }
+    ,{
+       
+        path: 'form-demande',
+        component: FormDemandeComponent
+
+    }
+
+    ,
+    {
+        path: 'rv',
+        component: RvComponent
+
+    }
+];
---
+++ src/app/features/demande-rv/demande-rv.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FormDemandeComponent } from './form-demande/form-demande.component';
+import { HeaderComponent } from '../../layouts/header/header.component';
+
+@Component({
+  selector: 'app-demande-rv',
+  imports: [],
+  templateUrl: './demande-rv.component.html',
+  styleUrl: './demande-rv.component.css'
+})
+export class DemandeRvComponent {
+
+}
