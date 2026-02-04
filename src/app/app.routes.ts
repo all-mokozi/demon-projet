c08468f88333ad3c9786823a79381439def19172
Mohamed Maiga <mohamed.maiga@ism.edu.sn>
2026-02-04 22:20:18 +0000

    premier commit sur livrable-cours2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './features/dashboard/dashboard.component';
 import { RvComponent } from './features/rv/rv.component';
 import { FormDemandeComponent } from './features/demande-rv/form-demande/form-demande.component';
+import { ListDemandeComponent } from './features/demande-rv/list-demande/list-demande.component';
 
 export const routes: Routes = [
     {
         path: '',
-        redirectTo: 'dashboard',
+        redirectTo: '/dashboard',
         pathMatch: 'full'
     }
     ,{
@@ -19,6 +20,10 @@ export const routes: Routes = [
         path: 'form-demande',
         component: FormDemandeComponent
 
+    }
+    ,{
+        path: 'list-demande',
+        component: ListDemandeComponent
     }
 
     ,
